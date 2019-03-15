a526277fe2ffb6845d6dee982a048e104fa6cb47
Deso <deso@xploers.com>
2019-03-15 18:03:24 +0800

    add

---
+++ approve-web/src/main/java/com/xdd/web/ApproveApplication.java
@@ -0,0 +1,16 @@
+package com.xdd.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Deso on 2019/3/15.
+ */
+@SpringBootApplication
+public class ApproveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApproveApplication.class, args);
+    }
+
+}
