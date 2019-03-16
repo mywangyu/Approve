72c599d58de5e3f43e7debb7ed73c8a4a4bbade6
Deso <deso@xploers.com>
2019-03-16 16:38:35 +0800

    add

--- approve-web/src/main/java/com/xdd/web/ApproveApplication.java
+++ approve-web/src/main/java/com/xdd/web/ApproveApplication.java
@@ -1,5 +1,6 @@
 package com.xdd.web;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Created by Deso on 2019/3/15.
  */
 @SpringBootApplication
+@MapperScan(basePackages = "")
 public class ApproveApplication {
 
     public static void main(String[] args) {
